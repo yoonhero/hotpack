a0642cbc2ea59483ba3d1e838317e9c827109eb2
yoonhero <yoonhero06@naver.com>
2022-12-19 21:49:42 +0900

    Deploy: Vercel Analytics

--- web/components/snow.js
+++ web/components/snow.js
@@ -80,12 +80,7 @@ const PrevSnowContainer = () => {
 };
 
 const SnowContainer = () => {
-    return (
-        // <div style={{ height: 400, width: 400, background: "#282c34", position: "relative" }}>
-        //     {/* <Snowfall /> */}
-        // </div>
-        <div></div>
-    );
+    return <Snowfall />;
 };
 
 export { SnowContainer };
