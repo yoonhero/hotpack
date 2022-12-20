9a0740b27b7d4358e59ba7e82d37e5f2865a88f9
yoonhero <yoonhero06@naver.com>
2022-12-20 09:19:29 +0900

    fix: Galaxy Clipboard

--- web/utils/clipboard.js
+++ web/utils/clipboard.js
@@ -1,6 +1,9 @@
+import copy from "copy-to-clipboard";
+
 const handleCopyClipBoard = async (text) => {
     try {
-        await navigator.clipboard.writeText(text);
+        // await navigator.clipboard.writeText(text);
+        copy(text);
     } catch (e) {
         console.log(e);
     }
